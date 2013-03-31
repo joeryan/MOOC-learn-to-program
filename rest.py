824c9a39dd1e86135a24d017d27efdbb65805524
joeryan <joe@clanryan.us>
2013-03-31 20:25:30 +0430

    Create read_dictionaries function

--- rest.py
+++ rest.py
@@ -71,13 +71,74 @@ def recommend(filename, price, cuisine):
     result = sort_by_rating(names_with_cuisine, names2rating)
     return result
 
+def read_dictionaries(filename):
+    """ (file) -> {str: int}, {str, list of str}, {str: list of str}
+        >>> read_dictionaries(restaurant_small)
+         {'Georgie Porgie': 87,
+         'Queen St. Cafe': 82,
+         'Dumplings R Us': 71,
+         'Mexican Grill': 85,
+         'Deep Fried Everything': 52}
 
+        {'$': ['Queen St. Cafe', 'Dumplings R Us', 'Deep Fried Everything'],
+         '$$': ['Mexican Grill'],
+         '$$$': ['Georgie Porgie'],
+         '$$$$': []}
 
+        {'Canadian': ['Georgie Porgie'],
+         'Pub Food': ['Georgie Porgie', 'Deep Fried Everything'],
+         'Malaysian': ['Queen St. Cafe'],
+         'Thai': ['Queen St. Cafe'],
+         'Chinese': ['Dumplings R Us'],
+         'Mexican': ['Mexican Grill']}
+    """
+    # initialize empty dictionaries
+    name2rating ={}
+    cuisine2name ={}
+    price2name = {'$': [], '$$': [], '$$$': [], '$$$$': []}
+        
+    # create dictionaries based on [name, rating, price, [type]]
+    count=0
+    for line in filename:
+        # strip off trailing newline
+        line = line.strip('\n')
+        # restaurant listing is 4 items long with a blank line
+        # decide which item we are on and store into temp variable
+        # when we reach 5th item (should be "") add to dicts
+        if count%5 == 0:
+            name=line
+        elif count%5 == 1:
+            # for rating strip ending %
+            rating = line.strip('%')
+        elif count%5 == 2:
+            price = line
+        elif count % 5 == 3:
+            # for cuisine: split into an array of str
+            cuisine =  line.split(',')
+        else:
+            name2rating[name]=rating
+            price2name[price].append(name)
+            for cuisine_name in cuisine:
+                # create new key if it doesn't exist, append if it does
+                if cuisine_name not in cuisine2name.keys():
+                    cuisine2name[cuisine_name] = [name]
+                else:
+                    cuisine2name[cuisine_name].append(name)
+        # increment how many lines we've read
+        count += 1
+        
+    # return completely built dictionaries
+    return (name2rating, price2name, cuisine2name)
+        
     
+        
+
+
 if __name__ == '__main__':
     # run test code if called from command line
     # Hard coded FILENAME
-    FILENAME = restaurant_small.txt
+    FILENAME = open('/home/joe/projects/python/MOOC-learn-to-program/restaurants_small.txt', 'r')
 
-    results = recommend(FILENAME, '$', ['Chinese', 'Thai'])
-    print results
+    name2rating, price2name, cusine2name = read_dictionaries(FILENAME)
+    # results = recommend(FILENAME, '$', ['Chinese', 'Thai'])
+    print (name2rating, price2name, cusine2name)
