3e53091c704982a9671faa02fc5f6811368845d9
joeryan <joe@clanryan.us>
2013-03-31 15:12:57 +0430

    create is_palindrome3 using 3rd palindrome algorithm

--- palindrome.py
+++ palindrome.py
@@ -31,6 +31,27 @@ def is_palindrome2(s):
     # omit middle char for odd length strings with int division
     return s[:n//2] == reverse(s[n-n//2:])
 
+def is_palindrome3(s):
+    """ (str) -> bool
+    >>> is_palindrome3('noon')
+    True
+    >>> is_palindrome3('racecar')
+    True
+    >>> is_palindrome3('dented')
+    False
+    """
+    # ensure string is all lowercase
+    s = s.lower()
+    # set counters to beginning and end of string
+    i, j = 0, len(s)-1
+    # move through string comparing characters from either end
+    while i < j and s[i] == s[j]:
+        i += 1
+        j -= 1
+
+    # return true if middle was reached and characters matched
+    return i >= j
+
 
 def reverse(s):
     """ (str) -> str
@@ -53,6 +74,7 @@ if __name__=='__main__':
     # if program run as stand alone, test each palindrome
     test_words=['noon', 'racecar', 'NoOn', 'dented']
     for word in test_words:
-        print (word, 'is palindrome', is_palindrome1(word))
-        print(word, 'is palindrome', is_palindrome2(word))
-    
+        print(word, "is palindrome (1)", is_palindrome1(word))
+        print(word, "is palindrome (2)", is_palindrome2(word))
+        print(word, "is palindrome (3)", is_palindrome3(word))
+        
