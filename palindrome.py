f10f2efa9df94ca4c4211fc21445107f1e1d04ae
joeryan <joe@clanryan.us>
2013-03-31 14:59:08 +0430

    create function using 2nd palindrome algorithm

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,6 @@
-import sys
-print( __name__)
 
-def is_palindrome(s):
+
+def is_palindrome1(s):
     """ (str) -> bool
     >>> is_palindrome('noon')
     True
@@ -10,7 +9,27 @@ def is_palindrome(s):
     >>> is_palindrome('dented')
     False
     """
+    # make sure the string is all lowercase
+    s=s.lower()
+    # test for palindrome by comparing to a reversed copy of the string
     return s==reverse(s)
+
+def is_palindrome2(s):
+    """ (str) -> bool
+    >>> is_palindrome2('noon')
+    True
+    >>> is_palindrome2('racecar')
+    True
+    >>> is_palindrome2('dented')
+    False
+    """
+    # make sure the string is all lowercase
+    s=s.lower()
+    # set n to only calculate s.len() once
+    n = len(s)
+    # compare first half of string with reverse of second half
+    # omit middle char for odd length strings with int division
+    return s[:n//2] == reverse(s[n-n//2:])
 
 
 def reverse(s):
@@ -22,15 +41,18 @@ def reverse(s):
     'a'
     """
     rev=''
+    
     for letter in s:
+        # push each letter onto the front of rev
         rev=letter+rev
         
     return rev
 
 
 if __name__=='__main__':
-    print(is_palindrome('noon'))
-    
-    print(is_palindrome('racecar'))
-    print(is_palindrome('dented'))
+    # if program run as stand alone, test each palindrome
+    test_words=['noon', 'racecar', 'NoOn', 'dented']
+    for word in test_words:
+        print (word, 'is palindrome', is_palindrome1(word))
+        print(word, 'is palindrome', is_palindrome2(word))
     
