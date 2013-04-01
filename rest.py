1ec4accc9b6a3528d642788314ceb4a7163766f4
Joe Ryan <joe@clanryan.us>
2013-04-01 08:00:22 +0430

    delete examples from docstrings, make them clear and concise

--- rest.py
+++ rest.py
@@ -8,16 +8,19 @@
         - a restaurant file that is open for reading
         - the price range (one of $, $$, $$$, and $$$$)
         - a list of cuisine types
-    return a list of restaurants (in the specified price range) and their
-    ratings from highest to lowest
+    return a list of restaurants (in the specified price range) and 
+    their ratings from highest to lowest
 
     restaurant file is text file of format:
         name
         rating
-        price (one of $, $$, $$$, $$$$)
+        price (one of '$', '$$', '$$$', '$$$$')
         list of cuisines served
         /n #blank line separates each restaurant
 
+    * Learn To Program course series from University of Toronto MOOC
+      puts examples in docstrings.  This clutters docstring for any
+      complex function/class and is not uutilized here
     * course uses list of lists, why not objects?
         - learn to program course hasn't covered object oriented programming yet
 
@@ -27,35 +30,10 @@
 
 def recommend(filename, price, cuisine):
     """ (file, str, list of str) -> list of str
-    given the sample data:
-        Restaurant name to rating:
-        # dict of {str: int}
-        {'Georgie Porgie': 87,
-         'Queen St. Cafe': 82,
-         'Dumplings R Us': 71,
-         'Mexican Grill': 85,
-         'Deep Fried Everything': 52}
 
-        Price to list of restaurant names:
-        # dict of {str, list of str}
-        {'$': ['Queen St. Cafe', 'Dumplings R Us', 'Deep Fried Everything'],
-         '$$': ['Mexican Grill'],
-         '$$$': ['Georgie Porgie'],
-         '$$$$': []}
-
-        Cuisine to list of restaurant names:
-        # dict of {str, list of str}
-        {'Canadian': ['Georgie Porgie'],
-         'Pub Food': ['Georgie Porgie', 'Deep Fried Everything'],
-         'Malaysian': ['Queen St. Cafe'],
-         'Thai': ['Queen St. Cafe'],
-         'Chinese': ['Dumplings R Us'],
-         'Mexican': ['Mexican Grill']}
-
-    With this data, for a price of '$' and cuisines of ['Chinese', 'Thai'], we
-    would produce this list:
-
-        [[82, 'Queen St. Cafe'], [71, 'Dumplings R Us']]
+    When provided a file opened for reading (of correct format), 
+      Price (one of '$'-'$$$$'), and a list of cuisines:
+      recommend returns a sorted list of restaurants and the ratings 
     """
 
     # read file into dictionaries
@@ -73,24 +51,10 @@ def recommend(filename, price, cuisine):
 
 def read_dictionaries(filename):
     """ (file) -> {str: int}, {str, list of str}, {str: list of str}
-        >>> read_dictionaries(restaurant_small)
-         {'Georgie Porgie': 87,
-         'Queen St. Cafe': 82,
-         'Dumplings R Us': 71,
-         'Mexican Grill': 85,
-         'Deep Fried Everything': 52}
 
-        {'$': ['Queen St. Cafe', 'Dumplings R Us', 'Deep Fried Everything'],
-         '$$': ['Mexican Grill'],
-         '$$$': ['Georgie Porgie'],
-         '$$$$': []}
-
-        {'Canadian': ['Georgie Porgie'],
-         'Pub Food': ['Georgie Porgie', 'Deep Fried Everything'],
-         'Malaysian': ['Queen St. Cafe'],
-         'Thai': ['Queen St. Cafe'],
-         'Chinese': ['Dumplings R Us'],
-         'Mexican': ['Mexican Grill']}
+    When given restaurant file in format listed in module docstring:
+    read_dictionaries returns the dictionaries containing:
+    {rest_name: rating}, {price: rest_names}, {rest_name: cuisines}
     """
     # initialize empty dictionaries
     name2rating ={}
