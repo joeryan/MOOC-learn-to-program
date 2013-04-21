c1fddc6104b344e353297a744da6e999cf8fd1a3
Joe Ryan <joe@clanryan.us>
2013-04-21 12:16:40 +0430

    initial draft of maze_tests.py

---
+++ assign2/maze_tests.py
@@ -0,0 +1,27 @@
+# File:                 : maze_tests.py
+# Author:               : Joe Ryan <joe@clanryan.us>
+# License:              : creative commons
+# Description:          : tests Rat Maze Game, Learn to Code 2 - Assignment 2
+# Arguments:            : none
+
+require a2
+require unittest
+
+class TestMazeClass(unittest.TestCase):
+    def setUp(self):
+        testmaze = [['#', '#', '#', '#', '#', '#','#'],
+                    ['#', '.', '.', '.', '.', '.', '#'],
+                    ['#', '.', '#', '#', '#', '.', '#'],
+                    ['#', '.', '.', '@', '#', '.', '#'],
+                    ['#', '@', '#', '.', '@', '.', '#'],
+                    ['#', '#', '#', '#', '#', '#', '#']]
+        
+        maze = a2.Maze([['#', '#', '#', '#', '#', '#','#'],
+                    ['#', '.', '.', '.', '.', '.', '#'],
+                    ['#', '.', '#', '#', '#', '.', '#'],
+                    ['#', '.', '.', '@', '#', '.', '#'],
+                    ['#', '@', '#', '.', '@', '.', '#'],
+                    ['#', '#', '#', '#', '#', '#', '#']],
+                       a2.Rat('J', 1, 1),
+                       a2.Rat('P', 1, 4))
+
